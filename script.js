65262184c1543a28b3606a3908754c423db83552
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-08 19:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let button=document.getElementById("enterBtn");
+button.addEventListener("click",()=>{
+	let para=document.getElementById("status");
+	let body=document.getElementsByTagName("body");
+	let hone=document.createElement("h1");
+	hone.innerText="Entered Metaverse";
+	para.replaceWith(hone)
+	
+	
+})
+
